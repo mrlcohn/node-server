8653950b9ee95690975e55b9ed8435d4e54d50c3
Leonard Cohn <leonardacohn@gmail.com>
2023-09-02 10:55:31 -0500

    Logic for GET request based on image path

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { getPhotos } from './db-scripts/get-random-photo'; 
+import { S3 } from '@aws-sdk/client-s3';
 
 const express = require('express');
 
@@ -10,11 +10,26 @@ app.listen(PORT, () => {
   console.log("Server listening on port " + PORT);
 });
 
-app.get("/randomPhoto", (req, res) => {
-  const photo = getPhotos();
+app.get("/photos/*", (req, res) => {
+  const path = req.originalUrl;
+
+  var s3 = new S3();
+  s3.getObject(
+    { Bucket: "picture-site-photos", Key: path },
+    function (error, data) {
+      if (error != null) {
+        res.status(404);
+      } else {
+        res.writeHead(200, { "Content-Type": "image/jpeg" });
+        res.send(data.Body);
+      }
+    }
+);
+
   res.status(200).send(photo);
 });
 
+/*
 app.post("/tshirt/:id", (req, res) => {
   const { id } = req.params;
   const { logo } = req.body;
@@ -27,3 +42,4 @@ app.post("/tshirt/:id", (req, res) => {
     tshirt: `With your ${logo} and ID of ${id}`,
   });
 });
+*/
