9856f0544ed6f4a61ca7b663743d57e8c54e6a1c
Leonard Cohn <leonardacohn@gmail.com>
2023-09-02 22:21:31 -0500

    Cleaning up responses

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+import AWS from 'aws-sdk';
 import express from 'express';
 
 const app = express();
 app.use(express.json());
+
+AWS.config.update({ "region": "us-east-2" });
 
 const PORT = 80;
 app.listen(PORT, () => {
@@ -12,24 +15,11 @@ app.listen(PORT, () => {
 
 app.get("/photos/\*", (req, res) => {
   const path = req.originalUrl.slice(7);
-
-  const client = new S3Client();
+  const client = new S3Client({ region: 'us-east-2' });
   const command = new GetObjectCommand({ Bucket: "picture-site-photos", Key: path });
-  try {
-    const url = getSignedUrl(client, command, { expiresIn: 30 });
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.send({ "src": url });
-  } catch {
-    console.log('Error occurred, investigate');
-    res.status(404).send({ "Message": "Error" });
-  }
 
-  if (error != null) {
-    res.status(404);
-  } else {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.send(url);
-  }
+  getSignedUrl(client, command, { expiresIn: 30 })
+    .then(response => res.send({ 'url': response }));
 });
 
 /*
