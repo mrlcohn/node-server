9eca07f45ad8d874e1bd64710a931bcb4ca12786
Leonard Cohn <leonardacohn@gmail.com>
2023-09-04 00:34:56 -0500

    First attempt at https

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 import AWS from 'aws-sdk';
+import cors from 'cors';
 import express from 'express';
+import https from 'https';
+import fs from 'fs';
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: '*',
+  methods: ['GET']
+}));
 
 AWS.config.update({ "region": "us-east-2" });
 
-const PORT = 80;
+const httpsServer = https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/server.cohn-family.photos/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/server.cohn-family.photos/fullchain.pem')
+}, app);
+
+const PORT = 443;
 app.listen(PORT, () => {
   console.log("Server listening on port " + PORT);
 });
 
 app.get("/photos/\*", (req, res) => {
-  const path = req.originalUrl.slice(7);
+  const path = req.originalUrl.slice(8);
   const client = new S3Client({ region: 'us-east-2' });
   const command = new GetObjectCommand({ Bucket: "picture-site-photos", Key: path });
 
