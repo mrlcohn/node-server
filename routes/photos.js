527d174a92a5985b42c66009d76f403d8bc8992c
Leonard Cohn <leonardacohn@gmail.com>
2023-09-05 21:59:23 -0500

    Syntax

--- routes/photos.js
+++ routes/photos.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const {
-  getAllPhotos,
-  getPhoto
-} = require('../controllers/photoController');
+const { getAllPhotos, getPhoto } = require('../controllers/photoController');
 const router = express.Router();
 
 // Get all photos
@@ -10,3 +7,5 @@ router.get("/", getAllPhotos);
 
 // Get the S3 URL to a specific photo
 router.get("/\*", getPhoto);
+
+module.exports = router;
